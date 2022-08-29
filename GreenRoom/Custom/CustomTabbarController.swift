ef096018ee242a33095047facca8d14edf7271aa
d0yvn <doyun0887@gmail.com>
2022-08-30 00:58:27 +0900

    :sparkles: add create button in tabor body: tabbar에 add버튼을 추가하는 Custom Tabbar controller 추가

---
+++ GreenRoom/Custom/CustomTabbarController.swift
@@ -0,0 +1,54 @@
+//
+//  CustomTabbarController.swift
+//  GreenRoom
+//
+//  Created by Doyun Park on 2022/08/30.
+//
+
+import UIKit
+
+final class CustomTabbarController: UITabBarController {
+    
+    private lazy var createButton = UIButton().then {
+        $0.backgroundColor = .mainColor
+        $0.setImage(UIImage(systemName: "plus"), for: .normal)
+        $0.contentMode = .scaleAspectFit
+        $0.imageView?.tintColor = .white
+        $0.layer.cornerRadius = view.frame.width / 14
+        $0.addTarget(self, action: #selector(didTapCreateQuestion), for: .touchUpInside)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initView()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+
+    private func initView() {
+        view.addSubview(createButton)
+        createButton.snp.makeConstraints { make in
+            make.top.equalTo(tabBar.snp.top).offset(-25)
+            make.centerX.equalToSuperview()
+            make.height.width.equalTo(view.frame.width / 7)
+        }
+    }
+    
+    func tabChangedTo(selectedIndex: Int) {
+        UIView.animate(withDuration: 0.5) {
+            self.createButton.isHidden = selectedIndex == 1
+        }
+    }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        UIView.animate(withDuration: 0.5) {
+            self.createButton.isHidden = item.tag != 1
+        }
+    }
+                
+    @objc func didTapCreateQuestion(){
+        print("didTapCreateQuestion")
+    }
+}
