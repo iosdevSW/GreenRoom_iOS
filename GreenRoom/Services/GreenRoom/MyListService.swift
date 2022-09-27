fc4ab62074911865245baba67515ca0ca1bcb83f
d0yvn <doyun0887@gmail.com>
2022-09-28 02:13:48 +0900

    feat: MyList의 답변 부분 API 일부 구현
    답변을 확인하는 부분 일부 구현 완료

--- GreenRoom/Services/GreenRoom/MyListService.swift
+++ GreenRoom/Services/GreenRoom/MyListService.swift
@@ -26,24 +26,65 @@ enum QuestionError: Error, LocalizedError {
     }
 }
 
-final class MyQuestionService {
+struct MyListWithAnswer: Codable {
+    let id: Int
+    let groupCategoryName, categoryName, question, answer: String
+    let keywords: [String]
+}
 
+final class MyListService {
+    
     func fetchMyQuestionList(completion:@escaping ((Result<[MyQuestion],Error>) -> Void)) {
         let url = URL(string: "\(Constants.baseURL)/api/my-questions")!
         
         AF.request(url, method: .get, encoding: URLEncoding.default, interceptor: AuthManager())
             .validate(statusCode: 200..<300)
             .responseDecodable(of: [MyQuestion].self) { response in
-            switch response.result {
-
-            case .success(let myQuestions):
-                completion(.success(myQuestions))
-            case .failure(let error):
-                completion(.failure(error))
+                switch response.result {
+                    
+                case .success(let myQuestions):
+                    completion(.success(myQuestions))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
             }
+    }
+    func fetchMyQuestion(id: Int) -> Observable<MyListWithAnswer> {
+        let url = URL(string: "\(Constants.baseURL)/api/my-questions/\(id)")!
+
+        return Observable.create { emitter in
+            AF.request(url, encoding: JSONEncoding.default, interceptor: AuthManager())
+                .validate(statusCode: 200..<300)
+                .responseDecodable(of: MyListWithAnswer.self) { response in
+                    switch response.result {
+                    case .success(let answer):
+                        emitter.onNext(answer)
+                    case .failure(let error):
+                        emitter.onError(error)
+                    }
+
+                }
+
+            return Disposables.create()
         }
     }
-    
+//    func fetchMyQuestion(id: Int, completion:@escaping((Result<MyListWithAnswer, Error>) -> Void)) {
+//        let url = URL(string: "\(Constants.baseURL)/api/my-questions/\(id)")!
+//
+//        AF.request(url, encoding: JSONEncoding.default, interceptor: AuthManager())
+//            .validate(statusCode: 200..<300)
+//            .responseString { response in
+//                print(response)
+//
+//                switch response.result {
+//                case .success(let result):
+//                    print(result)
+//                case .failure(let error):
+//                    print(error)
+//                }
+//
+//            }
+//    }
     func uploadQuestionList(categoryId: Int, question: String) -> Observable<Bool> {
         
         let url = URL(string: "\(Constants.baseURL)/api/my-questions")!
