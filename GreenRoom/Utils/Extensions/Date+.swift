d8b79b132bbfc7b8d4288a586585c8a5e9b0d313
d0yvn <doyun0887@gmail.com>
2022-10-01 15:34:14 +0900

    feat: Add Extension to Date, UITextView
    UITextView에 attributeText넣는 메소드에 Font까지 지정하는 기능 추가
    CreateGreenRoomViewController에서 Date값을 minutes으로 변환하는 기능 추가

--- GreenRoom/Utils/Extensions/Date+.swift
+++ GreenRoom/Utils/Extensions/Date+.swift
@@ -8,8 +8,19 @@
 import Foundation
 
 extension Date {
+    
     func adding(minutes: Int) -> Date {
         return Calendar.current.date(byAdding: .minute, value: minutes, to: self)!
+    }
+    
+    func getMinutes() -> Int {
+        let df = DateFormatter()
+        df.locale = Locale(identifier: "ko_KR")
+        df.dateFormat = "HH:mm"
+        
+        let minutes = df.string(from: self).components(separatedBy: ":")
+            .compactMap { Int($0) }
+        return minutes[0] * 60 + minutes[1]
     }
 }
 
