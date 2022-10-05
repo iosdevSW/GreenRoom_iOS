195f45237efb29c109760e7cb831171663714fcb
d0yvn <doyun0887@gmail.com>
2022-10-05 20:02:29 +0900

    refacotr: 검색ViewModel 내부 구조 변경
    검색 뷰모델 내부 구조 변경

--- GreenRoom/ViewModel/GreenRoom/Main/SearchViewModel.swift
+++ GreenRoom/ViewModel/GreenRoom/Main/SearchViewModel.swift
@@ -10,7 +10,7 @@ import RxSwift
 
 final class SearchViewModel: ViewModelType {
     
-    let greenroomService = GreenRoomService()
+    private let searchService = SearchService()
     
     var disposeBag = DisposeBag()
     
@@ -22,51 +22,26 @@ final class SearchViewModel: ViewModelType {
         let result: Observable<[SearchSectionModel]>
     }
     
-    private var recentKeywords = BehaviorSubject<[SearchSectionModel]>(value: [
-        SearchSectionModel.recent(header: "최근 검색어",items: [])
-    ])
-    
-    private var popularKeywords = BehaviorSubject<[SearchSectionModel]>(value: [
-        SearchSectionModel.popular(header: "인기 검색어",items: [])
-    ])
-    
     func transform(input: Input) -> Output {
-        input.trigger.subscribe(onNext: { [weak self] _ in
-            self?.fetchKeywords()
-        }).disposed(by: disposeBag)
         
-        let keywords = Observable.combineLatest(popularKeywords.asObserver(),
-                                                recentKeywords.asObserver()).map { $0.0 + $0.1 }
+        let output = input.trigger
+            .flatMap { _ in
+                return Observable.zip(self.searchService.fetchPopularKeywords().map { keywords in
+                    return [SearchSectionModel.popular(header: "인기 검색어",items: keywords.map { SearchSectionModel.Item(text: $0, type: .popular) })]
+                }, self.fetchRecentKeywords())
+            }.map { $0.0 + $0.1 }
+            
         
-        return Output(result: keywords)
+        return Output(result: output)
     }
     
-    private func fetchKeywords() {
-        recentKeywords.onNext(
-            [
-                SearchSectionModel.recent(
-                    header: "최근 검색어",
-                    items: CoreDataManager.shared.loadFromCoreData(request: RecentSearchKeyword.fetchRequest())
-                        .sorted { $0.date! > $1.date! }
-                        .map { SearchTagItem(text: $0.keyword!, type: .recent) })
-            ]
-        )
-        
-        
-        greenroomService.fetchPopularKeywords { [weak self] result in
-            switch result {
-            case .success(let keywords):
-                print("DEBUG: popular")
-                self?.popularKeywords.onNext([
-                    SearchSectionModel.popular(
-                            header: "인기 검색어",
-                            items: keywords.map { SearchSectionModel.Item(text: $0, type: .popular) }
-                        )]
-                )
-            case .failure(_):
-                break
-            }
-        }
+    private func fetchRecentKeywords() -> Observable<[SearchSectionModel]> {
+        return Observable.of([
+            SearchSectionModel.recent(
+                header: "최근 검색어",
+                items: CoreDataManager.shared.loadFromCoreData(request: RecentSearchKeyword.fetchRequest())
+                    .sorted { $0.date! > $1.date! }
+                    .map { SearchTagItem(text: $0.keyword!, type: .recent) })
+        ])
     }
-    
 }
